cb21d18965dced9bc6e8e052b36a7ec2791e3c13
Cameron Houser <cameronjhouser@gmail.com>
2017-05-26 11:59:39 -0400

    initial commit - files updated by James

---
+++ client/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reduxThunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Route, IndexRoute, Redirect, browserHistory} from 'react-router';
+import { AUTH_USER } from './actions/types';
+import App from './components/app';
+import Signin from './components/auth/signin';
+import Signout from './components/auth/signout';
+import Signup from './components/auth/signup';
+import RequireAuth from './components/auth/require_auth'
+import ListItem from './components/list/new-list-item';
+import ListsShow from './components/list/list-items';
+import ListShow from './components/list/list-show';
+import UpdateList from './components/list/update-list-item.js'
+import promise from 'redux-promise';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise, reduxThunk)(createStore);
+
+const store = createStoreWithMiddleware(reducers);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+		<Router history={browserHistory}>
+  			<Route path="/" component={App}>
+            <Route path="items" component={RequireAuth(ListsShow)}>
+            </Route>
+            <Route path="items/:id" component={RequireAuth(ListShow)} />
+            <Route path="updateitem/:id" component={RequireAuth(UpdateList)} />
+  			  	<Route path="signin" component={Signin} />
+    				<Route path="signout" component={Signout} />
+  	   			<Route path="signup" component={Signup} />
+  			  	<Route path="newitem" component={RequireAuth(ListItem)} />
+  			</Route>
+  		</Router>
+  </Provider>
+  , document.querySelector('.container'));
---
+++ server/router.js
@@ -0,0 +1,22 @@
+var Auth = require('./controllers/auth');
+var BucketList = require('./controllers/bucketlistcontroller');
+
+var passportService = require('./services/passport');
+var passport = require('passport');
+
+var requireAuth = passport.authenticate('jwt', {session: false});
+var requireSignin = passport.authenticate('local', {session: false});
+
+module.exports = function(app){
+	
+	app.post('/signup', Auth.signup);
+	app.post('/signin', requireSignin, Auth.signin);
+	app.post('/newitem', requireAuth, BucketList.addBucketList);
+
+	app.get('/items', requireAuth, BucketList.fetchBucketLists);
+	app.get('/items/:id', requireAuth, BucketList.fetchBucketList);
+
+	app.put('/items/:id', requireAuth, BucketList.updateBucketList);
+
+	app.delete('/items/:id', requireAuth, BucketList.deleteBucketList);
+}
